9c52385551c3ca4cd9677cf4fbf4ea887391f2f5
wz <17600820823@163.com>
2023-09-18 10:56:55 +0800

    多种方式导包

--- src/gocode/study_go_02/main.go
+++ src/gocode/study_go_02/main.go
@@ -5,6 +5,10 @@ import (
   "go-study/src/gocode/study_go_02/lib2"
 )
 
+// 导包方式-匿名  _ "go-study/src/gocode/study_go_02/lib1"
+// 导包方式-别名  myLib1 "go-study/src/gocode/study_go_02/lib1"
+// 导包方式-全量导入  * "go-study/src/gocode/study_go_02/lib1"
+
 // 主包的init
 func init()  {
 fmt.Println("main init...")
